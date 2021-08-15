da355a81bead271bef97a8491f7a1352a2e7abc9
Freshenext <39339295+Freshenext@users.noreply.github.com>
2021-08-15 10:55:41 -0400

    Create customAxios.js

---
+++ customAxios.js
@@ -0,0 +1,44 @@
+import axios from "axios";
+import {store} from "../redux/store";
+
+
+//const  urlDev = 'http://localhost:3000'; // MUST CHANGE
+
+
+function getUserToken(){
+    return store.getState().user.token || 0;
+}
+
+function getAuthorizationHeader(){
+    const token = getUserToken();
+    if(token)
+        return `Bearer ${token}`
+    else
+        return '';
+}
+
+export function getAxios(){
+    const axiosInstance = axios.create({
+        baseURL: urlDev,
+        timeout: 5000,
+    });
+
+    /* Request */
+    axiosInstance.interceptors.request.use(req => {
+        const tokenAuth = getAuthorizationHeader();
+        if(tokenAuth !== ''){
+            req.headers.Authorization = tokenAuth;
+        }
+        return req;
+    });
+    /* Response */
+    axiosInstance.interceptors.response.use(function(response){
+        /* On success = status code 200 */
+        return response.data;
+    }, function(error){
+        /* On failure = status code <> 200 */
+        return Promise.reject(error);
+    });
+
+    return axiosInstance;
+}
